1aec54ded0ba7956c343d09b573007c077f64b32
McProbe <62525217+McProbe@users.noreply.github.com>
2020-06-09 01:13:32 -0700

    Fixed possible issues with different drives

--- Server Starter.bat
+++ Server Starter.bat
@@ -1,3 +1,3 @@
 TITLE FiveM Server
-cd /d C:\"insert directory to server data folder here"
-C:\"insert directory to server folder"\FXServer.exe +exec server.cfg
+cd /d "insert directory to server data folder here"
+"insert directory to server folder"\FXServer.exe +exec server.cfg
